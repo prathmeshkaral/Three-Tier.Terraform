d2fb6d1200ae551da81a315f3b56672e4efff3e3
PrathmeshKarale <162536182+prathmeshkaral@users.noreply.github.com>
2024-06-22 13:43:21 +0530

    Update student_form.tf

--- student_form.tf
+++ student_form.tf
@@ -232,7 +232,7 @@ resource "aws_instance" "Database" {
     }
   
 }
-#Attach the security group to tomcat instance
+#Attach the security group to database instance
 resource "aws_network_interface_sg_attachment" "database-sg" {
     security_group_id = aws_security_group.ssh.id
     network_interface_id = aws_instance.Database.primary_network_interface_id
